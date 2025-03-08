ac0bd70269a8c816f935aebb0275f2b805cce250
hahn2003mc <hahn2003mc@gmail.com>
2025-03-08 16:29:26 -0500

    added placeholder value and script on click of calculate button

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function CalculateBMR()
 
 }
 
+document.getElementById('calculate_BMR_button').addEventListener('click', function()
+{
+    // get inputs
+    var weight = document.getElementById('weight').value;
+    var feet = document.getElementById('height_feet').value;
+    var inches = document.getElementById('height_inches').value;
+
+    var resultText = 55; // placeholder value
+    document.getElementById('result').innerHTML = resultText;
+});
