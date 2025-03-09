9e77d6ce935f8ee7fb435db41eeb4d150ca19bd1
hahn2003mc <hahn2003mc@gmail.com>
2025-03-09 12:51:34 -0400

    BMR bug fix 2; added changes from codepen

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-function CalculateBMR()
-{
-
-}
-
 document.getElementById("calculate_BMR_button").addEventListener("click", function()
 {
     // get inputs
-    var weight = document.getElementById("weight").value;
-    var feet = document.getElementById("height_feet").value;
-    var inches = document.getElementById("height_inches").value;
-    var age = document.getElementById("age").value;
+    var weight = parseInt(document.getElementById("weight").value, 10);
+    var feet = parseInt(document.getElementById("height_feet").value, 10);
+    var inches = parseInt(document.getElementById("height_inches").value, 10);
+    var age = parseInt(document.getElementById("age").value, 10);
     var sex = document.getElementById("sex").value;
-
-    var BMR = error;
-    if (typeof(weight) == typeof(feet) == typeof(inches) == number)
+    var BMR = "error";
+    if (sex = "female")
     {
-        if (sex = "female")
-        {
-            BMR = 655 + (4.35*weight) + (4.7*(12*feet + inches)) - (4.7*age);
-        }
-        else
-        {
-            BMR = 66 + (6.23*weight) + (12.7*(12*feet + inches)) - (6.8*age);
-        }
+      BMR = 655 + (4.35*weight) + (4.7*(12*feet + inches)) - (4.7*age);
+    }
+    else
+    {
+      BMR = 66 + (6.23*weight) + (12.7*(12*feet + inches)) - (6.8*age);
     }
     document.getElementById("result").innerHTML = BMR;
 });
