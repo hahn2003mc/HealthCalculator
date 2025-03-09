a9f8ddbce6a56f8944551db105d8b1525036632e
hahn2003mc <hahn2003mc@gmail.com>
2025-03-09 13:03:58 -0400

    BMR output displaying properly

--- script.js
+++ script.js
@@ -6,14 +6,23 @@ document.getElementById("calculate_BMR_button").addEventListener("click", functi
     var inches = parseInt(document.getElementById("height_inches").value, 10);
     var age = parseInt(document.getElementById("age").value, 10);
     var sex = document.getElementById("sex").value;
-    var BMR = "error";
-    if (sex = "female")
+    console.log(typeof(weight));
+    console.log(typeof(feet));
+    console.log(typeof(inches));
+    if (sex == "female")
     {
-      BMR = 655 + (4.35*weight) + (4.7*(12*feet + inches)) - (4.7*age);
+      BMR = Math.round(655 + (4.35*weight) + (4.7*(12*feet + inches)) - (4.7*age));
     }
     else
     {
-      BMR = 66 + (6.23*weight) + (12.7*(12*feet + inches)) - (6.8*age);
+      BMR = Math.round(66 + (6.23*weight) + (12.7*(12*feet + inches)) - (6.8*age));
     }
-    document.getElementById("result").innerHTML = BMR;
+    if (isNaN(BMR))
+      {
+        document.getElementById("result").innerHTML = "Sorry, I didn't get that. Are you sure you have filled in all fields?";
+      }
+    else
+    {
+    document.getElementById("result").innerHTML = "Without exercising, your body burns approximately " + BMR + " calories a day, on average.";
+  }
 });
