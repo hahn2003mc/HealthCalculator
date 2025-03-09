f06e59d278d66fe7ac22c6700381a1ce639a75b8
hahn2003mc <hahn2003mc@gmail.com>
2025-03-09 12:37:03 -0400

    BMR bug fix

--- script.js
+++ script.js
@@ -12,17 +12,17 @@ document.getElementById("calculate_BMR_button").addEventListener("click", functi
     var age = document.getElementById("age").value;
     var sex = document.getElementById("sex").value;
 
+    var BMR = error;
     if (typeof(weight) == typeof(feet) == typeof(inches) == number)
     {
         if (sex = "female")
         {
-            var BMR = 655 + (4.35*weight) + (4.7*(12*feet + inches)) - (4.7*age);
+            BMR = 655 + (4.35*weight) + (4.7*(12*feet + inches)) - (4.7*age);
         }
         else
         {
-            var BMR = 66 + (6.23*weight) + (12.7*(12*feet + inches)) - (6.8*age);
+            BMR = 66 + (6.23*weight) + (12.7*(12*feet + inches)) - (6.8*age);
         }
-        var resultText = 55; // placeholder value
-        document.getElementById("result").innerHTML = resultText;
     }
+    document.getElementById("result").innerHTML = BMR;
 });
