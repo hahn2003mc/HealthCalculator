eafdc0a3573f1bac8fc34ebaf8b703f32271111e
hahn2003mc <hahn2003mc@gmail.com>
2025-03-09 12:32:18 -0400

    added BMR calculation in js

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ function CalculateBMR()
 
 }
 
-document.getElementById('calculate_BMR_button').addEventListener('click', function()
+document.getElementById("calculate_BMR_button").addEventListener("click", function()
 {
     // get inputs
-    var weight = document.getElementById('weight').value;
-    var feet = document.getElementById('height_feet').value;
-    var inches = document.getElementById('height_inches').value;
+    var weight = document.getElementById("weight").value;
+    var feet = document.getElementById("height_feet").value;
+    var inches = document.getElementById("height_inches").value;
+    var age = document.getElementById("age").value;
+    var sex = document.getElementById("sex").value;
 
-    var resultText = 55; // placeholder value
-    document.getElementById('result').innerHTML = resultText;
+    if (typeof(weight) == typeof(feet) == typeof(inches) == number)
+    {
+        if (sex = "female")
+        {
+            var BMR = 655 + (4.35*weight) + (4.7*(12*feet + inches)) - (4.7*age);
+        }
+        else
+        {
+            var BMR = 66 + (6.23*weight) + (12.7*(12*feet + inches)) - (6.8*age);
+        }
+        var resultText = 55; // placeholder value
+        document.getElementById("result").innerHTML = resultText;
+    }
 });
